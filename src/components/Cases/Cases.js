270e6503fb9fd2650e8428c64fcf0d3be562dc96
ShahidaMahtab <marwa.mahtab@gmail.com>
2021-10-19 22:28:27 +0600

    responsive

--- src/components/Cases/Cases.js
+++ src/components/Cases/Cases.js
@@ -9,19 +9,19 @@ const Cases = () => {
       .then((data) => setCases(data));
   }, []);
   return (
-    <Container className="my-5 px-4 mx-auto">
-      <div className="mb-5 py-5">
+    <Container className="my-lg-5 mx-auto pt-lg-5">
+      <div className="mb-5">
         <p className="pt-4 lh-1 text-center">
           <small className="text-clr text-capitalize fw-bold">
             cases that we solved
           </small>
         </p>
-        <h2 className="header-text fw-bold text-center mb-0">
+        <h2 className="header-text fw-bold text-center pb-3">
           We have Solved More Than 90 % Sases Successfully
         </h2>
       </div>
       <div className="">
-        <Row xs={1} md={2} lg={4} className="mx-auto mb-5">
+        <Row xs={1} md={2} lg={4} className="mx-auto g-lg-4">
           {cases.map((cs) => (
             <Case cases={cs} key={cs.id}></Case>
           ))}
